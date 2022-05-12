c87300c96985479585c1042e5c3f3b650d8c95a2
Fedef11980 <federicof1@gmail.com>
2022-05-12 16:29:41 -0300

    JQuery

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
-$(document).ready(inicio);
+$(document).ready(inicio());
 
 function inicio() {
-  $("#imgplayers").mouseenter(cambiarImg);
-  $("#imgplayers").mouseleave(originalImg);
+  $("#imgplayers").mouseenter(cambiarImg());
+  $("#imgplayers").mouseleave(originalImg());
 }
 
 function cambiarImg() {
-  $("#imgplayers").attr("src", "../img/2016/leonTomeo2.jpg");
+  $("#imgplayers").src = '../img/2016/leonTomeo2.jpeg';
   console.log("cambiar", cambiarImg);
 }
 
 function originalImg() {
-  $("#imgplayers").attr("src", "../img/2016/leonTomeo.jpeg");
+  $("#imgplayers").src = '../img/2016/leonTomeo.jpeg';
   console.log("original", originalImg);
 }
