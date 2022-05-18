8107a501c3518fdd898340b79f4b76806aff272a
Fedef11980 <federicof1@gmail.com>
2022-05-18 10:22:20 -0300

    Arreglar imagen de jugadores

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
+
 $(document).ready(inicio());
 
 function inicio() {
-  $("#imgplayers").mouseenter(cambiarImg());
-  $("#imgplayers").mouseleave(originalImg());
+  $("#imgplayers").mouseenter(cambiarImg);
+  $("#imgplayers").mouseleave(originalImg);
 }
 
 function cambiarImg() {
-  $("#imgplayers").src = '../img/2016/leonTomeo2.jpeg';
-  console.log("cambiar", cambiarImg);
+  $("#imgplayers img").attr("src",'../img/2016/leonTomeo2.jpg');
 }
 
 function originalImg() {
-  $("#imgplayers").src = '../img/2016/leonTomeo.jpeg';
-  console.log("original", originalImg);
+  $("#imgplayers img").attr("src",'../img/2016/leonTomeo.jpeg')
 }
