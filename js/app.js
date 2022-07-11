e777da0125f08ee5ebf4cbaa5f191f51015b97cf
Fedef11980 <federicof1@gmail.com>
2022-07-11 12:55:06 -0300

    actualizacion

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
+$(document).ready(function() {
+// Cuando entra mouse, obtengo la data-src-secundaria y la pongo como src  
+  $('.fotocambia').mouseenter(function () {
+    var srcSecundaria = $(this).attr('data-src-secundaria');
+    $(this).attr('src', srcSecundaria);
+  });
+  // Cuando sale mouse, obtengo la data-src-primaria y la pongo como src
+  $('.fotocambia').mouseleave(function () {
+    var srcPrimaria = $(this).attr('data-src-primaria');
+    $(this).attr('src', srcPrimaria);
+  });
 
-$(document).ready(inicio());
-
-function inicio() {
-  $("#imgplayers").mouseenter(cambiarImg);
-  $("#imgplayers").mouseleave(originalImg);
-}
-
-function cambiarImg() {
-  $("#imgplayers img").attr("src",'../img/2016/leonTomeo2.jpg');
-}
-
-function originalImg() {
-  $("#imgplayers img").attr("src",'../img/2016/leonTomeo.jpeg')
-}
+});
